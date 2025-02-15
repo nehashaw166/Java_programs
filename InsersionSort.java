184a00501d4c8a40dff0549f5fd11dc07e777a71
Neha Shaw <66564555+nehashaw166@users.noreply.github.com>
2025-02-15 06:51:31 +0000

    dummy

--- InsersionSort.java
+++ InsersionSort.java
@@ -3,19 +3,22 @@ import java.util.Arrays;
 public class InsersionSort {
     public static void main(String[] args) {
        int [] a = {2,8,4,9,3,6 , 8, 9, 6, 12, 23, 47,6} ;
-       int temp; 
+       int temp;
+       //int f; 
        for(int i = 1; i<a.length; i++){
+        System.out.println(i);
             for(int j=i-1;j>=0; j--){
-                if(a[i]<a[j]){
+                if(a[j+1]<a[j]){
                     temp= a[j];
-                    a[j]= a[i];
-                    a[i]=temp;
-                    i=6;
+                    a[j]= a[j+1];
+                    a[j+1]=temp;
                 }
-            }
+            
        }
+    }
        System.out.println(Arrays.toString(a));
 
     }
     
 }
+
